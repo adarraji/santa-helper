31b29739c68bbac1a3d31d71e6df65164ed1dbf4
adarraji <a.darraji@outlook.com>
2022-07-01 13:31:25 -0400

    Added comments

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const fs = require("fs");
 
+// 1 - What floor does Santa en up on
+// ( --> go up 1 floor
+// ) --. go down 1 floor
 const question1 = () => {
     fs.readFile("./santa.txt", (err, data) => {
         if (err) {
