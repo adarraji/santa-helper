5b676179c7dd53158c224355270f3cda4ea5b85d
adarraji <a.darraji@outlook.com>
2022-07-01 13:46:33 -0400

    Added code for part 2

--- script.js
+++ script.js
@@ -18,3 +18,27 @@ const question1 = () => {
 question1();
 
 
+// 2 - When does Santa enter the basement
+const question2 = () => {
+    fs.readFile("./santa.txt", (err, data) => {
+        if (err) {
+            console.log(err);
+        }
+        const inputArray = data.toString("utf8").split("");
+        let accumulator = 0;
+        let counter = 0;
+        const answer = inputArray.some(currentItem => {
+            if (currentItem === "(") {
+                accumulator += 1
+            }
+            else if (currentItem === ")") {
+                accumulator -= 1
+            }
+            counter++;
+            return accumulator < 0;
+        });
+        console.log("basement entered at: ", counter);
+    })
+}
+
+question2();
